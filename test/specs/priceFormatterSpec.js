b490131f3226e8b6e7138855ba6b0a3987606ccf
Bryan Ingle <bryan.ingle@barchart.com>
2016-03-23 13:28:44 -0500

    Change decimal formatting 377.75 --> "3.7775"

--- test/specs/priceFormatterSpec.js
+++ test/specs/priceFormatterSpec.js
@@ -3,6 +3,36 @@ var PriceFormatter = require('../../lib/priceFormatter');
 describe('When a price formatter is created', function() {
     var priceFormatter;
 
+    describe('with a decimal fraction separator', function() {
+        beforeEach(function() {
+            priceFormatter = new PriceFormatter('.');
+        });
+
+        it('formats 377 (with unit code 2) as "3.7700"', function() {
+            expect(priceFormatter.format(377, '2')).toEqual('3.7700');
+        });
+
+        it('formats 377.5 (with unit code 2) as "3.7750"', function() {
+            expect(priceFormatter.format(377.5, '2')).toEqual('3.7750');
+        });
+
+        it('formats 377.75 (with unit code 2) as "3.7775"', function() {
+            expect(priceFormatter.format(377.75, '2')).toEqual('3.7775');
+        });
+
+        it('formats undefined (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(undefined, '2')).toEqual('');
+        });
+
+        it('formats null (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(null, '2')).toEqual('');
+        });
+
+        it('formats Number.NaN (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(Number.NaN, '2')).toEqual('');
+        });
+    });
+
     describe('with a dash fraction separator and no special fractions', function() {
         beforeEach(function() {
             priceFormatter = new PriceFormatter('-', false);

e58bae721a3cf892c04307fddc6d7a783bf8de48
Bryan Ingle <bryan.ingle@barchart.com>
2016-06-13 15:26:57 -0500

    Add "short" time formatter for 24-hour clock

--- test/specs/timeFormatterSpec.js
+++ test/specs/timeFormatterSpec.js
@@ -200,6 +200,106 @@ describe('When a time formatter is created (and a 24-hour clock is specified)', 
 	});
 });
 
+describe('When a time formatter is created (and a "short" 24-hour clock is specified)', function() {
+	var tf;
+
+	beforeEach(function() {
+		tf = timeFormatter(false, true);
+	});
+
+	describe('and a quote is formatted (with no "flag" and a "lastPrice" value)', function() {
+		var quote;
+
+		beforeEach(function() {
+			quote = {
+				lastPrice: 123.456
+			};
+		});
+
+		describe('and the quote time is midnight on May 3, 2016', function() {
+			beforeEach(function() {
+				quote.time = new Date(2016, 4, 3, 0, 0, 0);
+			});
+
+			it('the formatter outputs "00:00"', function() {
+				expect(tf.format(quote)).toEqual('00:00');
+			});
+		});
+
+		describe('and the quote time is noon on May 3, 2016', function() {
+			beforeEach(function() {
+				quote.time = new Date(2016, 4, 3, 12, 0, 0);
+			});
+
+			it('the formatter outputs "12:00"', function() {
+				expect(tf.format(quote)).toEqual('12:00');
+			});
+		});
+
+		describe('and the quote time is 7:08:09 AM on May 3, 2016', function() {
+			beforeEach(function() {
+				quote.time = new Date(2016, 4, 3, 7, 8, 9);
+			});
+
+			it('the formatter outputs "07:08"', function() {
+				expect(tf.format(quote)).toEqual('07:08');
+			});
+		});
+
+		describe('and the quote time is 1:08:09 PM on May 3, 2016', function() {
+			beforeEach(function() {
+				quote.time = new Date(2016, 4, 3, 13, 8, 9);
+			});
+
+			it('the formatter outputs "13:08"', function() {
+				expect(tf.format(quote)).toEqual('13:08');
+			});
+		});
+
+		describe('and the quote time is 1:08:09 PM and a timezone is present', function() {
+			beforeEach(function() {
+				quote.time = new Date(2016, 4, 3, 13, 8, 9);
+				quote.timezone = 'EDT';
+			});
+
+			it('the formatter outputs "13:08"', function() {
+				expect(tf.format(quote)).toEqual('13:08 EDT');
+			});
+		});
+	});
+
+	describe('and a quote is formatted (with with a "flag" and a "lastPrice" value)', function() {
+		var quote;
+
+		beforeEach(function() {
+			quote = {
+				lastPrice: 123.456,
+				flag: 'p'
+			};
+		});
+
+		describe('and the quote time is midnight on May 3, 2016', function() {
+			beforeEach(function() {
+				quote.time = new Date(2016, 4, 3, 0, 0, 0);
+			});
+
+			it('the formatter outputs "05/03/16"', function() {
+				expect(tf.format(quote)).toEqual('05/03/16');
+			});
+		});
+
+		describe('and the quote time is noon on May 3, 2016', function() {
+			beforeEach(function() {
+				quote.time = new Date(2016, 4, 3, 12, 0, 0);
+			});
+
+			it('the formatter outputs "05/03/16"', function() {
+				expect(tf.format(quote)).toEqual('05/03/16');
+			});
+		});
+	});
+});
+
 describe('When a time formatter is created (and a 12-hour clock is specified)', function() {
 	var tf;
 

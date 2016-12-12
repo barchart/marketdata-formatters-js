ce3193abb2b4e9032920642473ca2653f4d2e11b
Bryan Ingle <bryan.ingle@barchart.com>
2016-12-12 14:16:03 -0600

    Add function to "standardize" symbols (map ESZ16, ESZ2016 --> ESZ6)

--- test/specs/symbolParserSpec.js
+++ test/specs/symbolParserSpec.js
@@ -11,3 +11,37 @@ describe('When checking the display format for the symbol "UERMNTUS.RT"', functi
 		expect(symbolParser.displayUsingPercent('UERMNTUS.RT')).toEqual(true);
 	});
 });
+
+describe('When getting a standardized symbol', function() {
+	it('TSLA should map to TSLA', function() {
+		expect(symbolParser.getStandardSymbol('TSLA')).toEqual('TSLA');
+	});
+
+	it('TSLA.BZ should map to TSLA.BZ', function() {
+		expect(symbolParser.getStandardSymbol('TSLA.BZ')).toEqual('TSLA.BZ');
+	});
+
+	it('ESZ6 should map to ESZ6', function() {
+		expect(symbolParser.getStandardSymbol('ESZ6')).toEqual('ESZ6');
+	});
+
+	it('ESZ16 should map to ESZ6', function() {
+		expect(symbolParser.getStandardSymbol('ESZ16')).toEqual('ESZ6');
+	});
+
+	it('ESZ2016 should map to ESZ6', function() {
+		expect(symbolParser.getStandardSymbol('ESZ16')).toEqual('ESZ6');
+	});
+
+	it('ES*0 should map to ES*0', function() {
+		expect(symbolParser.getStandardSymbol('ES*0')).toEqual('ES*0');
+	});
+
+	it('$DOWI should map to $DOWI', function() {
+		expect(symbolParser.getStandardSymbol('$DOWI')).toEqual('$DOWI');
+	});
+
+	it('^EURUSD should map to ^EURUSD', function() {
+		expect(symbolParser.getStandardSymbol('^EURUSD')).toEqual('^EURUSD');
+	});
+});

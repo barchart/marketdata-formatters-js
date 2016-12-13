d5164421ebd85141de9b031dc31e3448044e49e2
Bryan Ingle <bryan.ingle@barchart.com>
2016-12-13 09:50:36 -0600

    Release. Bump version number

--- test/dist/barchart-marketdata-utilities-tests-1.0.js
+++ test/dist/barchart-marketdata-utilities-tests-1.0.js
@@ -373,10 +373,23 @@ module.exports = function() {
 	'use strict';
 
 	var percentRegex = /(\.RT)$/;
+	var jerqFutureConversionRegex = new RegExp('([A-Z0-9]{1,3})([A-Z]{1})([0-9]{3}|[0-9]{1})?([0-9]{1})$');
 
 	return {
 		displayUsingPercent: function(symbol) {
 			return percentRegex.test(symbol);
+		},
+
+		getStandardSymbol: function(symbol) {
+			var returnRef;
+
+			if (typeof symbol === 'string') {
+				returnRef = symbol.replace(jerqFutureConversionRegex, '$1$2$4');
+			} else {
+				returnRef = null;
+			}
+
+			return returnRef;
 		}
 	};
 }();
@@ -1504,6 +1517,40 @@ describe('When checking the display format for the symbol "HPIUSA.RP"', function
 describe('When checking the display format for the symbol "UERMNTUS.RT"', function() {
 	it('it should be formatted as a percent', function() {
 		expect(symbolParser.displayUsingPercent('UERMNTUS.RT')).toEqual(true);
+	});
+});
+
+describe('When getting a standardized symbol', function() {
+	it('TSLA should map to TSLA', function() {
+		expect(symbolParser.getStandardSymbol('TSLA')).toEqual('TSLA');
+	});
+
+	it('TSLA.BZ should map to TSLA.BZ', function() {
+		expect(symbolParser.getStandardSymbol('TSLA.BZ')).toEqual('TSLA.BZ');
+	});
+
+	it('ESZ6 should map to ESZ6', function() {
+		expect(symbolParser.getStandardSymbol('ESZ6')).toEqual('ESZ6');
+	});
+
+	it('ESZ16 should map to ESZ6', function() {
+		expect(symbolParser.getStandardSymbol('ESZ16')).toEqual('ESZ6');
+	});
+
+	it('ESZ2016 should map to ESZ6', function() {
+		expect(symbolParser.getStandardSymbol('ESZ16')).toEqual('ESZ6');
+	});
+
+	it('ES*0 should map to ES*0', function() {
+		expect(symbolParser.getStandardSymbol('ES*0')).toEqual('ES*0');
+	});
+
+	it('$DOWI should map to $DOWI', function() {
+		expect(symbolParser.getStandardSymbol('$DOWI')).toEqual('$DOWI');
+	});
+
+	it('^EURUSD should map to ^EURUSD', function() {
+		expect(symbolParser.getStandardSymbol('^EURUSD')).toEqual('^EURUSD');
 	});
 });
 },{"../../lib/symbolParser":7}],17:[function(require,module,exports){
